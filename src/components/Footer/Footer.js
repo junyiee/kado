bff1c3a131e6cf37e959ebbdd818a557fbe312e5
Jun <jennycheng816@gmail.com>
2023-02-22 15:03:55 -0500

    newsletter customization

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,36 +1,45 @@
 import React from 'react'
 import './Footer.css'
+import { Link } from 'react-router-dom'
 
 function Footer() {
     return (
         <footer>
             <div className='footer-container'>
                 <section className='newsletter'>
-                    <h3>Newsletter</h3>
-                    <p>Subscribe for updates!</p>
-                    <div className='footer-form'>
-                        <form>
-                            <input
-                                type='email'
-                                name='email'
-                                placeholder='Your Email'
-                            />
-                            <input
-                                type='text'
-                                name='name'
-                                placeholder='Your Name'
-                            />
-                            <button type='submit'>Subscribe</button>
-                        </form>
-                    </div>
+                    <form className='footer-form'>
+                        <h3>Newsletter</h3>
+                        <p>Subscribe for updates!</p>
+                        <input
+                            className='form-input'
+                            type='email'
+                            name='email'
+                            placeholder='Your Email'
+                        />
+                        <button type='submit'>Subscribe</button>
+                    </form>
                 </section>
                 <div>
-                    <ul>
-                        <li>About Us</li>
-                        <li>Contact Us</li>
+                    <ul className='footer-list'>
+                        <li>
+                            <Link to='/' className='footer-links'>About Us</Link>
+                        </li>
+                        <li>
+                            Contact Us
+                        </li>
                     </ul>
                     <section className='socials'>
-                        Socials
+                        <ul className='socials-list'>
+                            <li className='socials-item'>
+                                <Link to='/' className='socials-links'>Discord</Link>
+                            </li>
+                            <li className='socials-item'>
+                                <Link to='/' className='socials-links'>Instagram</Link>
+                            </li>
+                            <li className='socials-item'>
+                                <Link to='/' className='socials-links'>Youtube</Link>
+                            </li>
+                        </ul>
                     </section>
                     <small>Junyiee &copy; 2023</small>
                 </div>
