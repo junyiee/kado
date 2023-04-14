f812f9809cc99e5be9a07fd15f67a9ab1f26e537
Jun <jennycheng816@gmail.com>
2023-04-14 00:17:21 -0400

    modified footer layout

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -2,7 +2,7 @@ import React from 'react'
 import './Footer.css'
 import { Link } from 'react-router-dom'
 import { FaDiscord, FaInstagram, FaYoutube } from 'react-icons/fa'
-import logo from '../../assets/olive.svg'
+// import logo from '../../assets/olive.svg'
 
 function Footer() {
     return (
@@ -10,8 +10,8 @@ function Footer() {
             <div className='footer-wrap'>
                 <section className='newsletter'>
                     <form className='footer-form'>
-                        <h3>Newsletter</h3>
-                        <p>Subscribe for updates!</p>
+                        <h4>Newsletter</h4>
+                        <p>Subscribe for updates, restocks, upcoming projects and more!</p>
                         <input
                             className='footer-input'
                             type='email'
@@ -21,37 +21,59 @@ function Footer() {
                         <button type='submit'>Subscribe</button>
                     </form>
                 </section>
-                <section className='contact-us'>
+                <section className="footer-menu">
+                    <ul className="footer-list">
+                        <li className="footer-item">
+                            <Link to='/' className='footer-links'>
+                                About Us
+                            </Link>
+                        </li>
+                        <li className="footer-item">
+                            <Link to='/' className='footer-links'>
+                                FAQs
+                            </Link>
+                        </li>
+                        <li className="footer-item">
+                            <Link to='/' className='footer-links'>
+                                Contact Us
+                            </Link>
+                        </li>
+                    </ul>
+                </section>
+                <section className="socials">
+                    <h4>Follow Us</h4>
+                    <ul className='socials-list'>
+                        <li>
+                            <Link to='/' className='socials-links'>
+                                <FaDiscord />
+                            </Link>
+                        </li>
+                        <li>
+                            <Link to='/' className='socials-links'>
+                                <FaInstagram />
+                            </Link>
+                        </li>
+                        <li>
+                            <Link to='/' className='socials-links'>
+                                <FaYoutube />
+                            </Link>
+                        </li>
+                    </ul>
+                </section>
+                {/* <section className='contact-us'>
                     <h4>Contact Us</h4>
                     JUNNY
                     <br />
                     Mon-Fri 10am-8pm
                     <br />
                     jimbo@mail.com
-                </section>
+                </section> */}
             </div>
-            <section className='socials'>
-                <Link to='/' className="logo-link">
+            <section className='bottom'>
+                {/* <Link to='/' className="logo-link">
                     <img src={logo} alt="olive icon" className="logo" />olive
-                </Link>
+                </Link> */}
                 <small className='rights'>Junyiee &copy; 2023</small>
-                <ul className='socials-list'>
-                    <li className='socials-item'>
-                        <Link to='/' className='socials-links'>
-                            <FaDiscord />
-                        </Link>
-                    </li>
-                    <li className='socials-item'>
-                        <Link to='/' className='socials-links'>
-                            <FaInstagram />
-                        </Link>
-                    </li>
-                    <li className='socials-item'>
-                        <Link to='/' className='socials-links'>
-                            <FaYoutube />
-                        </Link>
-                    </li>
-                </ul>
             </section>
         </footer>
     )
