a1cbdc1eae9277a6e0947d04189b433e46f684c9
Jun <jennycheng816@gmail.com>
2023-04-20 21:02:24 -0400

    fixed cards layout issue on resized screens

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -8,7 +8,7 @@ function Navbar() {
     const [click, setClick] = useState(false);
 
     const handleClick = () => setClick(!click);
-    const closeMobileMenu = () => setClick(false)
+    const closeMobileMenu = () => setClick(false);
 
     return (
         <nav className='navbar'>
