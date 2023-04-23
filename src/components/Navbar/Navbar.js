a24eaf04c9aea15e7bca7df229c1788f7ff10e51
Jun <jennycheng816@gmail.com>
2023-04-22 21:05:16 -0400

    made navbar mobile friendly

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -3,6 +3,7 @@ import './Navbar.css'
 import { Link } from 'react-router-dom'
 import { FaTimes, FaBars, FaShoppingCart, FaRegUser } from 'react-icons/fa'
 import logo from '../../assets/olive.svg'
+import { NavLink } from 'react-router-dom'
 
 function Navbar() {
     const [click, setClick] = useState(false);
@@ -13,33 +14,68 @@ function Navbar() {
     return (
         <nav className='navbar'>
             <div className='navbar-container'>
-                <Link to='/' className='navbar-logo'>
+                <Link to='/' className='navbar-logo' onClick={closeMobileMenu}>
                     <img src={logo} alt="olive icon" className="logo" />olive
                 </Link>
                 <div className="menu-icon" onClick={handleClick}>
-                    <i className={click ? <FaTimes /> : <FaBars />} />
+                    {click ? <FaTimes color='#fff' /> : <FaBars color='#fff' />}
                 </div>
                 {/* set active state of mobile navbar */}
                 <ul className={click ? 'nav-menu active' : 'nav-menu'}>
-                    <li className='nav-item'>
-                        <Link to='/shop' className='nav-links' onClick={closeMobileMenu}>
+                    <li className="nav-item">
+                        <NavLink
+                            to="/"
+                            className={({ isActive }) =>
+                                "nav-links" + (isActive ? " activated" : "")
+                            }
+                            onClick={closeMobileMenu}
+                        >
+                            Home
+                        </NavLink>
+                    </li>
+                    <li className="nav-item">
+                        <NavLink
+                            to="/shop"
+                            className={({ isActive }) =>
+                                "nav-links" + (isActive ? " activated" : "")
+                            }
+                            onClick={closeMobileMenu}
+                        >
                             Shop
-                        </Link>
+                        </NavLink>
                     </li>
-                    <li className='nav-item'>
-                        <Link to='/' className='nav-links' onClick={closeMobileMenu}>
+                    <li className="nav-item">
+                        <NavLink
+                            to="/"
+                            className={({ isActive }) =>
+                                "nav-links" + (isActive ? " activated" : "")
+                            }
+                            onClick={closeMobileMenu}
+                        >
                             Updates
-                        </Link>
+                        </NavLink>
                     </li>
-                    <li className='nav-item'>
-                        <Link to='/login' className='nav-links' onClick={closeMobileMenu}>
+                    <li className="nav-item">
+                        <NavLink
+                            to="/login"
+                            className={({ isActive }) =>
+                                "nav-links" + (isActive ? " activated" : "")
+                            }
+                            onClick={closeMobileMenu}
+                        >
                             <FaRegUser />
-                        </Link>
+                        </NavLink>
                     </li>
-                    <li className='nav-item'>
-                        <Link to='/cart' className='nav-links' onClick={closeMobileMenu}>
+                    <li className="nav-item">
+                        <NavLink
+                            to="/cart"
+                            className={({ isActive }) =>
+                                "nav-links" + (isActive ? " activated" : "")
+                            }
+                            onClick={closeMobileMenu}
+                        >
                             <FaShoppingCart />
-                        </Link>
+                        </NavLink>
                     </li>
                 </ul>
             </div>
